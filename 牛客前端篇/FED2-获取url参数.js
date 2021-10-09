e399bf17a38b94e45bb8448805301f60a18a1ae7
axing521 <axing@bupt.edu.cn>
2021-10-09 15:58:26 +0800

    delete：无用的草稿

--- 牛客前端篇/FED2-获取url参数.js
+++
@@ -1,7 +0,0 @@
-function getUrlParam(sUrl, sKey) {
-    var result = {}
-    sUrl.replace(/\??(\w+)=(\w+)&?/g, function(a, k, v){
-        result[k] = result[k]? [...result[k]].concat(v) : v
-    })
-    return sKey? result[sKey]? result[sKey] : '' : result
-}
--- 牛客前端篇/FED6-斐波那契数列.js
+++
@@ -1,3 +0,0 @@
-function fibonacci(n) {
-    return n===1||n===2 ? 1 : fibonacci(n-1)+fibonacci(n-2);
-}
