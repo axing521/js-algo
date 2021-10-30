91e4a714e4005e0ed2b34a2332c76ff360a6a5c2
axing521 <axing@bupt.edu.cn>
2021-10-30 17:11:49 +0800

    modify: LC-145🛠

--- leetcode-ACBash/数据结构/二叉树/145-二叉树的后序遍历.js
+++ leetcode-ACBash/数据结构/二叉树/145-二叉树的后序遍历.js
@@ -2,11 +2,11 @@
  * @creater:ACBash
  * @create_time:21-10-26 20:29:21
  * @last_modify:ACBash
- * @modify_time:21-10-26 21:29:18
+ * @modify_time:21-10-30 17:11:8
  * @line_count:36
  **/
 
-/* 经典递归 */
+/* 1.经典递归 */
 const postorderTraversal = (root) => {
     let ans = [];
 
@@ -23,20 +23,20 @@ const postorderTraversal = (root) => {
     return ans;
 };
 
-/* 用栈迭代模拟递归|后序遍历的代码和前序/中序不一样！！ */
+/* 2.用栈迭代模拟递归|后序遍历的代码和前序/中序不一样！！ */
 const postorderTraversal = (root) => {
     if(!root) return [];
-    let ans = [], stack = [root], node = root;
+    let ans = [], stack = [root], node = root; //stack是待访问节点栈，node存储已遍历节点。
 
     while(stack.length){
         const cur = stack[stack.length-1];
 
-        if((cur.left == null && cur.right == null) || cur.left == node || cur.right == node){
-            node = stack.pop();
-            ans.push(node.val);    
+        if((!cur.left && !cur.right) || cur.left == node || cur.right == node){
+            node = stack.pop(); //node更新为最新的遍历节点
+            ans.push(node.val);
         }else{
             if(cur.right) stack.push(cur.right);
-            if(cur.left) stack.push(cur.left);
+            if(cur.left) stack.push(cur.left);  //这里写的顺序也很讲究
         }
     }
 
