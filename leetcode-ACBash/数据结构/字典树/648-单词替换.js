bea8ebafd3ce0151ea03f90bc3feaa49e3cce4d9
axing521 <axing@bupt.edu.cn>
2021-11-09 21:29:53 +0800

    put: LC-648✅

---
+++ leetcode-ACBash/数据结构/字典树/648-单词替换.js
@@ -0,0 +1,172 @@
+/***
+ * @creater:ACBash
+ * @create_time:21-11-8 20:46:53
+ * @last_modify:ACBash
+ * @modify_time:21-11-9 21:28:57
+ * @line_count:164
+ **/
+
+/* 暴力法，API怪，你还别说，api还真就是爹，84ms */
+const shortestRoot = (word, dictionary) => {
+    let shortest = Infinity, ans = word;
+
+    for(const root of dictionary){
+        if(root.length < shortest && word.startsWith(root)){
+            ans = root;
+            shortest = root.length;
+        }
+    }
+
+    return ans;
+};
+
+const replaceWords = (dictionary, sentence) => {
+    const words = sentence.split(" ");
+
+    const replaced = words.map(val => shortestRoot(val, dictionary));
+
+    const ans = replaced.join(" ");
+    
+    return ans;
+};
+
+/* 字典树模板化，150ms，因为对每个单词都构造字典树，优化思路可以从复用字典树入手 */
+class Trie{
+    constructor(){
+        this.dictionary = {};
+    }
+
+    insert(word){
+        let node = this.dictionary;
+
+        for(const c of word){
+            if(!node[c]) node[c] = {"isEnd": false};
+            node = node[c];
+        }
+
+        node["isEnd"] = true;
+    }
+
+    search(word){
+        let node = this.dictionary;
+
+        for(const c of word){
+            if(!node[c]) return false;
+            node = node[c];
+        }
+
+        return node["isEnd"];
+    }
+
+    startsWith(prefix){
+        let node = this.dictionary;
+
+        for(const c of prefix){
+            if(!node[c]) return false;
+            node = node[c];
+        }
+
+        return true;
+    }
+}
+
+const shortestRoot = (word, dictionary) => {
+    let shortest = Infinity, ans = word, trie = new Trie();
+    trie.insert(word);
+
+    for(const root of dictionary){
+        if(trie.startsWith(root) && root.length < shortest){
+            ans = root;
+            shortest = root.length;
+        }
+    }
+
+    return ans;
+};
+
+const replaceWords = (dictionary, sentence) => {
+    const words = sentence.split(" ");
+
+    const replaced = words.map(val => shortestRoot(val, dictionary));
+
+    const ans = replaced.join(" ");
+    
+    return ans;
+};
+
+/* 字典树思想,bug干掉了 */
+const shortestRoot = (word, trie) => {
+    let node = trie;
+
+    for(const c of word){
+        if(!node[c]) return word;
+        else if(!node[c][isEnd]) node = node[c];
+        else return node[c]["val"];
+    }
+
+    return word;
+};
+
+const insert = (word, trie) => {
+    let node = trie;
+
+    for(const c of word){
+        if(!node[c]) node[c] = {"isEnd": false};
+        node = node[c];
+    }
+
+    node["isEnd"] = true;
+    node["val"] = word;
+}
+
+const replaceWords = (dictionary, sentence) => {
+    let trie = {};
+    for(const root of dictionary) insert(root, trie);
+
+    const words = sentence.split(" ");
+
+    const replaced = words.map(val => shortestRoot(val, trie));
+
+    const ans = replaced.join(" ");
+
+    return ans;
+};
+
+/* LC,相当于精简上面的做法 */
+class Trie{
+    constructor(){
+        this.dictionary = {};
+    }
+
+    insert(word){
+        let node = this.dictionary;
+
+        for(const c of word){
+            if(!node[c]) node[c] = {"isEnd": false, "word": word};
+            node = node[c];
+        }
+
+        node["isEnd"] = true;
+        node["word"] = word;
+    }
+
+    search(word){
+        let node = this.dictionary;
+
+        for(const c of word){
+            if(!node[c]) return word;
+            else if(!node[c]["isEnd"]) node = node[c];
+            else return node[c]["word"];
+        }
+
+        return word;
+    }
+}
+
+const replaceWords = (dictionary, sentence) => {
+    let trie = new Trie();
+
+    for(const root of dictionary) trie.insert(root);
+
+    return sentence.split(" ").map(word => trie.search(word)).join(" ");
+};
