57ab80480d98d6be17e22e2f6464a93811a7555d
axing521 <axing@bupt.edu.cn>
2021-11-19 20:51:02 +0800

    put: LC-979

---
+++ leetcode-ACBash/DFS/979-在二叉树中分配硬币.js
@@ -0,0 +1,26 @@
+/***
+ * @creater:ACBash
+ * @create_time:21-11-19 18:59:4
+ * @last_modify:ACBash
+ * @modify_time:21-11-19 20:49:57
+ * @line_count:18
+ **/
+
+ const distributeCoins = (root) => {
+    let ans = 0;
+
+    const dfs = (node) => {
+        if(!node) return 0;
+
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        
+        ans += Math.abs(left) + Math.abs(right);    //需要调整的，多了或者少了
+
+        return node.val + left + right - 1; //老爹，我这儿需要调整的节点数是这么多
+    };
+
+    dfs(root);
+
+    return ans;
+};
