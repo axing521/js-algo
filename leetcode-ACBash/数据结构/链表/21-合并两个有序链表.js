1dc8eb62d5ee840b5831c4c467cd56e56c86a367
axing521 <axing@bupt.edu.cn>
2021-12-06 19:01:54 +0800

    put: LC-21✅

---
+++ leetcode-ACBash/数据结构/链表/21-合并两个有序链表.js
@@ -0,0 +1,58 @@
+/***
+ * @creater:ACBash
+ * @create_time:21-12-6 18:9:13
+ * @last_modify:ACBash
+ * @modify_time:21-12-6 19:1:16
+ * @line_count:50
+ **/
+
+ const mergeTwoLists = (list1, list2) => {
+    let node = new ListNode();
+    const dummy = node;
+
+    while(list1 || list2){
+        if(list1 && list2){
+            if(list1.val > list2.val){
+                node.next = list2;
+                node = node.next;
+                list2 = list2.next;
+            }else{
+                node.next = list1;
+                node = node.next;
+                list1 = list1.next;
+            }
+        }else if(!list1){
+            node.next = list2;
+            node = node.next;
+            list2 = list2.next;
+        }else if(!list2){
+            node.next = list1;
+            node = node.next;
+            list1 = list1.next;
+        }
+    }
+
+    return dummy.next;
+};
+
+/* 更优雅的写法 */
+const mergeTwoLists = (list1, list2) => {
+    let node = new ListNode();
+    const dummy = node;
+
+    while(list1 && list2){
+        if(list1.val > list2.val){
+            node.next = list2;
+            list2 = list2.next;
+        }else{
+            node.next = list1;
+            list1 = list1.next;
+        }
+        node = node.next;
+    }
+
+    if(list1) node.next = list1;
+    if(list2) node.next = list2;
+
+    return dummy.next;
+};
