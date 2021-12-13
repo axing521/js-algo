704c6b2dde4136bca2a9647909b93cf32baeb660
axing521 <axing@bupt.edu.cn>
2021-12-13 10:47:09 +0800

    modify: LC-105🛠

--- leetcode-ACBash/105-从前序与中序遍历序列构造二叉树.js
+++ leetcode-ACBash/105-从前序与中序遍历序列构造二叉树.js
@@ -2,8 +2,8 @@
  * @creater:ACBash
  * @create_time:21-12-9 17:41:10
  * @last_modify:ACBash
- * @modify_time:21-12-12 21:59:54
- * @line_count:21
+ * @modify_time:21-12-13 10:46:43
+ * @line_count:35
  **/
 
 /* 先序遍历用来找根节点，根节点作为stop，遍历中序遍历的节点加入左右子树 */
@@ -26,4 +26,18 @@ const buildTree = (preorder, inorder) => {
     }
 
     return build();
+};
+
+/* 分而治之，归并的感觉，好理解，但是用到API，时间空间复杂度会提高 */
+const buildTree = (preorder, inorder) => {
+    if(!inorder.length) return null;
+
+    let root = new TreeNode(preorder[0]);
+
+    const i = inorder.indexOf(root.val);
+
+    root.left = buildTree(preorder.slice(1, i + 1), inorder.slice(0, i));
+    root.right = buildTree(preorder.slice(i + 1), inorder.slice(i + 1));
+
+    return root;
 };
