d5ead72b47fd1ce79fc197ad37b548851542bd9a
axing521 <axing@bupt.edu.cn>
2021-12-14 17:42:40 +0800

    modify: LC-110🛠

--- leetcode-ACBash/数据结构/二叉树/110-平衡二叉树.js
+++ leetcode-ACBash/数据结构/二叉树/110-平衡二叉树.js
@@ -2,8 +2,8 @@
  * @creater:ACBash
  * @create_time:21-11-1 15:41:36
  * @last_modify:ACBash
- * @modify_time:21-11-1 17:2:47
- * @line_count:25
+ * @modify_time:21-12-14 17:42:23
+ * @line_count:32
  **/
 
 /* 和104-二叉树的最大深度有点联系，这个是高度，自下而上 */
@@ -11,23 +11,30 @@
 const isBalanced = (root) => {
     const getHeight = (node) => {
         if(!node) return 0;
+
         let leftHeight = getHeight(node.left);
         let rightHeight = getHeight(node.right);
 
         if(leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) return -1;
-        else return 1 + Math.max(leftHeight, rightHeight);
+        
+        return 1 + Math.max(leftHeight, rightHeight);
     };
 
-    return getHeight(root) === -1 ? false : true;
+    return getHeight(root) == -1 ? false : true;
 };
 
 /* 和104-二叉树的最大深度有点联系，这个是高度，自上而下 */
-const maxDepth = (root) => {
-    if(!root) return 0;
-    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+/* 树的高度是根结点的最大深度 */
+const maxDepth = (node) => {
+    if(!node) return 0;
+
+    return 1 + Math.max(maxDepth(node.left), maxDepth(node.right));
 };
 
 const isBalanced = (root) => {
     if(!root) return true;
-    return Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    
+    if(Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) return false;
+
+    return isBalanced(root.left) && isBalanced(root.right);
 };
