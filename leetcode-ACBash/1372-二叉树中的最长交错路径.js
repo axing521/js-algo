5b277ee37f9745d76864957719f9f8ea23cb88eb
axing521 <axing@bupt.edu.cn>
2022-01-16 23:53:50 +0800

    put: LC-1372✅

---
+++ leetcode-ACBash/1372-二叉树中的最长交错路径.js
@@ -0,0 +1,22 @@
+/***
+ * @creater:ACBash
+ * @create_time:22-1-16 22:56:31
+ * @last_modify:ACBash
+ * @modify_time:22-1-16 23:53:6
+ * @line_count:14
+ **/
+
+/* 关键是任意节点（除开根节点）只有一个父节点，从他是往左/右走来的？ */
+const longestZigZag = (root) => {
+    let ans = 0;
+
+    const dfs = (node, left, right) => {
+        ans = Math.max(ans, left, right);
+        node.left && dfs(node.left, right + 1, 0);
+        node.right && dfs(node.right, 0, left + 1);
+    };
+
+    dfs(root, 0, 0);    //后两个参数表示来的方向的路径已经存储的长度值
+
+    return ans;
+};
