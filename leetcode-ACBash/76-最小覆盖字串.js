15907963e8a35f08afcf1fe91f682bf92ae4eb58
axing521 <axing@bupt.edu.cn>
2022-03-29 19:25:58 +0800

    modify: LC-76🛠

--- leetcode-ACBash/76-最小覆盖字串.js
+++ leetcode-ACBash/76-最小覆盖字串.js
@@ -1,3 +1,11 @@
+/***
+ * @creater:ACBash
+ * @create_time:22-3-29 19:25:41
+ * @last_modify:ACBash
+ * @modify_time:22-3-29 19:25:41
+ * @line_count:106
+ **/
+
 //滑动窗口
 /* 265/266,最后一个超时了，可惜 */
 const minWindow = (s, t) => {
@@ -68,3 +76,39 @@ const minWindow = (s,t) => {
     if(start===s.length) return "";
     else return s.substring(start,start+len);
 }
+
+const minWindow = (s, t) => {
+    let map = {}, cNum = 0;
+    
+    for(const c of t){
+        if(!map[c]){
+            map[c] = 0;
+            cNum++;
+        } 
+        map[c]++;
+    }
+
+    let slow = 0, minSubstr = [-Infinity, -1], flag = false;    //注意为什么要这样初始化minSubstr，因为这样能够最大化区间，并且在找不到子串时能返回“”，另外注意门控信号flag
+    
+    for(let fast = 0; fast < s.length; fast++){
+        if(map[s[fast]] != undefined){
+            map[s[fast]]--;
+            
+            if(map[s[fast]] == 0) cNum--;
+            
+            if(cNum == 0) flag = true;
+
+            while(cNum == 0){
+                if(map[s[slow]] != undefined) map[s[slow]]++;
+                if(map[s[slow]] == 1) cNum++;
+                slow++;
+            }
+
+            if(flag && fast - slow + 1 < minSubstr[1] - minSubstr[0]) minSubstr = [slow - 1, fast];
+
+            flag = false;
+        }
+    }
+
+    return s.substring(minSubstr[0], minSubstr[1] + 1);
+};
