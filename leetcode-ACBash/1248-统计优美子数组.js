bcdd587152e6a1e7da891cf4063c4966296b9edb
axing521 <axing@bupt.edu.cn>
2022-04-03 16:44:44 +0800

    modify: LC-1248🛠

--- leetcode-ACBash/1248-统计优美子数组.js
+++ leetcode-ACBash/1248-统计优美子数组.js
@@ -1,3 +1,11 @@
+/***
+ * @creater:ACBash
+ * @create_time:22-4-3 16:44:33
+ * @last_modify:ACBash
+ * @modify_time:22-4-3 16:44:33
+ * @line_count:55
+ **/
+
 /* 2021/10/8 20：31 自己写的滑动窗口，注意两个if块的顺序 */
 const numberOfSubarrays = (nums,k) => {
     let left = 0, right = 0, len = nums.length, winMeetOdd = 0, ret = 0, leftTemp = 0;
@@ -30,3 +38,26 @@ const numberOfSubarrays = (nums,k) => {
 };
 
 console.log(numberOfSubarrays([2,4,6],1));
+
+const numberOfSubarrays = (nums, k) => {
+    let ans = 0, slow1 = 0, oddNum1 = 0, slow2 = 0, oddNum2 = 0;
+
+    for(let fast = 0; fast < nums.length; fast++){
+        if(nums[fast] % 2 == 1) oddNum1++;
+        if(nums[fast] % 2 == 1) oddNum2++;
+
+        while(slow1 <= fast && oddNum1 > k){
+            if(nums[slow1] % 2 == 1) oddNum1--;
+            slow1++;
+        }
+
+        while(slow1 <= fast && oddNum2 >= k){
+            if(nums[slow2] % 2 == 1) oddNum2--;
+            slow2++;
+        }
+
+        ans += slow2 - slow1;
+    }
+
+    return ans;
+};
