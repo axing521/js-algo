cf55aa6a1620ae7407277ab51a11b17b249552e2
axing521 <axing@bupt.edu.cn>
2022-04-03 15:54:11 +0800

    modify: LC-1004🛠

--- leetcode-ACBash/滑动窗口/1004-最大连续1的个数Ⅲ.js
+++ leetcode-ACBash/滑动窗口/1004-最大连续1的个数Ⅲ.js
@@ -1,3 +1,11 @@
+/***
+ * @creater:ACBash
+ * @create_time:22-4-3 15:53:53
+ * @last_modify:ACBash
+ * @modify_time:22-4-3 15:53:54
+ * @line_count:34
+ **/
+
 /* LC:找最大窗口，rsum=>花的，lsum=>挣的，存款=>k */
 const longestOnes = (nums,k) => {
     const len = nums.length;
@@ -11,6 +19,23 @@ const longestOnes = (nums,k) => {
             left++;
         }
         ans = Math.max(ans, right-left+1);
+    }
+
+    return ans;
+};
+
+const longestOnes = (nums,k) => {
+    let ans = 0, slow = 0;
+
+    for(let fast = 0; fast < nums.length; fast++){
+        if(nums[fast] == 0) k--;
+
+        while(k < 0){
+            if(nums[slow] == 0) k++;
+            slow++;
+        }
+
+        ans = Math.max(ans, fast - slow + 1);
     }
 
     return ans;
