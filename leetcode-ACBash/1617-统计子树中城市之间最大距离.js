086328bd85f0a369c5ca8ce3bfa7032ce11b3522
axing521 <axing@bupt.edu.cn>
2022-05-14 20:17:18 +0800

    2022-5-7-15-53

--- leetcode-ACBash/1617-统计子树中城市之间最大距离.js
+++ leetcode-ACBash/1617-统计子树中城市之间最大距离.js
@@ -2,9 +2,83 @@
  * @creater:ACBash
  * @create_time:21-11-28 12:3:21
  * @last_modify:ACBash
- * @modify_time:21-11-28 21:9:24
- * @line_count:105
+ * @modify_time:22-5-14 20:17:5
+ * @line_count:179
  **/
+
+ const countSubgraphsForEachDiameter = (n, edges) => {
+    //建多源最短路径矩阵
+    let minPaths = Array.from({length: n}, () => new Array(n).fill(16));
+    
+    //自回点为0
+    for(let i = 0; i < n; i++){
+        minPaths[i][i] = 0;
+    }
+
+    //edges 初始化 矩阵minPaths
+    for(const [u, v] of edges){
+        minPaths[u - 1][v - 1] = 1;
+        minPaths[v - 1][u - 1] = 1;
+    }
+
+    //floyd完成minPaths
+    for(let k = 0; k < n; k++){
+        for(let i = 0; i < n; i++){
+            for(let j = 0; j < n; j++){
+                if(minPaths[i][k] != 16 && minPaths[k][j] != 16){
+                    minPaths[i][j] = Math.min(minPaths[i][j], minPaths[i][k] + minPaths[k][j]);
+                }
+            }
+        }
+    }
+
+    //DP, 二进制
+    let dp = new Array(1 << n).fill(0);
+    
+    //edges 初始化 DP, 比如 1 ，2 节点 => 0011 状态 存储最大距离
+    for(const [u, v] of edges){
+        dp[(1 << (u - 1)) | (1 << (v - 1))] = 1;
+    }
+
+    //状态转移
+    for(let j = 1; j < dp.length; j++){
+        if(dp[j] == 0) continue;
+        
+        for(let i = 0; i < n; i++){
+            //找与当前状态的子树的新增节点(1 << i), j & (1 << i) != 0 确保是新节点, dp[j | (1 << i)] != 0 确保不重计算新子树状态
+            if((j & (1 << i)) != 0 || dp[j | (1 << i)] != 0) continue;
+
+            for(let k = 0; k < n; k++){
+                //在旧子树节点中寻找新增节点的邻居节点, 转移新子树状态
+                if((j & (1 << k)) != 0 && minPaths[i][k] == 1){
+                    dp[j | (1 << i)] = dp[j];
+                    break;
+                }
+            }
+
+            //判断是否已转移新子树状态
+            if(dp[j | (1 << i)] == 0) continue;
+
+            //奇怪的是，感觉下面有点多此一举？下次看看
+            for(let kk = 0; kk < n; kk++){
+                //更新最大距离
+                if((j & (1 << kk)) != 0){
+                    dp[j | (1 << i)] = Math.max(dp[j | (1 << i)], minPaths[i][kk]);
+                }
+            }
+        }
+    }
+
+    let ans = new Array(n - 1).fill(0);
+
+    for(const maxDist of dp){
+        if(maxDist != 0){
+            ans[maxDist - 1]++;
+        }
+    }
+
+    return ans;
+};
 
  const countSubgraphsForEachDiameter = (n, edges) => {
     const all_distances = (n, edges) => {
