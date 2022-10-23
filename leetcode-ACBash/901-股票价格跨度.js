7ca52c349f77a7496338a6ee2695d4eed9156717
axing521 <axing@bupt.edu.cn>
2022-10-23 18:28:23 +0800

    2022-10-23-901

--- leetcode-ACBash/901-股票价格跨度.js
+++ leetcode-ACBash/901-股票价格跨度.js
@@ -2,54 +2,52 @@
  * @creater:ACBash
  * @create_time:21-10-18 15:3:2
  * @last_modify:ACBash
- * @modify_time:21-10-18 16:7:6
- * @line_count:47
+ * @modify_time:22-10-23 18:27:48
+ * @line_count:45
  **/
 
 /* 单调栈，600ms */
+//按题意模拟
 class StockSpanner{
     constructor(){
-        this.prices = [];
+        this.container = [];
     }
 
-    next(price){
-        let pointer = this.prices.length;
-        while(pointer && this.prices[pointer-1] <= price){
-            pointer--;
-        }
-        this.prices.push(price);
+    next(val){
+        this.container.push(val);
+        
+        let ans = 1, index = this.container.length - 2;
 
-        return this.prices.length - pointer;
+        while(index >= 0 && this.container[index] <= val){
+            ans++;
+            index--;
+        }
+
+        return ans;
     }
 }
 
 /* 单调栈，空间换时间，增加一个count记录当前跨度，300ms */
 class StockSpanner{
     constructor(){
-        this.stack = [];
+        this.container = [];
         this.count = 0;
     }
 
-    next(price){
-        while(this.stack.length && this.stack[this.stack.length-1].value <= price){
-            this.stack.pop();
-        }    
-        let tmp = this.stack.length ? this.stack[this.stack.length-1].index : 0;
+    next(val){
+        while(this.container.length && this.container[this.container.length - 1].val <= val){
+            this.container.pop();
+        }
+
+        const flag = this.container.length ? this.container[this.container.length - 1].index : 0;
+
         this.count++;
-        this.stack.push({
+
+        this.container.push({
             "index": this.count,
-            "value": price
+            "val": val
         })
 
-        return this.count - tmp;
+        return this.count - flag;
     }
 }
-
-let a = new StockSpanner();
-console.log(a.next(100));
-console.log(a.next(80));
-console.log(a.next(60));
-console.log(a.next(70));
-console.log(a.next(60));
-console.log(a.next(75));
-console.log(a.next(85));
